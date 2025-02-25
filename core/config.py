568bf62cbe1a5ee3e242262ecae24eb630e30410
noahal1 <noahall127@outlook.com>
2025-02-25 15:07:34 +0800

    修正数据库URL用户名

--- core/config.py
+++ core/config.py
@@ -2,4 +2,4 @@ import os
 from dotenv import load_dotenv
 load_dotenv() 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:magna123@10.227.122.217:3306/tj") 
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://tjm:magna123@10.227.122.217:3306/tj") 
